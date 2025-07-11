bf3d2a2b6f02e1c7981a414e618efc3c57945935
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-07-11 17:45:34 +0530

    Feat: Add TRPC API routes and client integration
    - Created `src/app/api/trpc/[trpc]/route.ts` to handle TRPC requests for GET and POST methods.
    - Introduced `src/trpc/client.tsx` for TRPC client setup with React Query integration and superjson support.
    - Added `src/trpc/server.tsx` to configure TRPC server options and caching for query clients.

---
+++ src/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,14 @@
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { appRouter } from "@/trpc/routers/_app";
+import { createTRPCContext } from "@/trpc/init";
+
+const handler = (request: Request) => {
+  return fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req: request,
+    router: appRouter,
+    createContext: createTRPCContext,
+  });
+};
+
+export { handler as GET, handler as POST };
